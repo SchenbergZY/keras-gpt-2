8fb3329a6928b73728b09fef4a23f270d5b843f6
Yue Zhang <34243681+SchenbergZY@users.noreply.github.com>
2022-10-23 02:40:25 +0200

    add notations to this gen func

--- keras_gpt_2/gen.py
+++ keras_gpt_2/gen.py
@@ -17,24 +17,24 @@ def generate(model,
     :param temperature: Randomness in boltzmann distribution.
     :return: A list of generated texts.
     """
-    batch_size = len(texts)
-    encodes = [bpe.encode(text) for text in texts]
-    text_lens = [len(encode) for encode in encodes]
-    max_len = max(text_lens)
-    input_data = [encode + [0] * (max_len - len(encode)) for encode in encodes]
-    for shift in range(length):
-        output_data = model.predict(np.array(input_data))
-        for index in range(batch_size):
-            probs = [(prob, i) for i, prob in enumerate(output_data[index, text_lens[index] + shift - 1])]
-            probs.sort(reverse=True)
-            probs = probs[:top_k]
-            indices, probs = list(map(lambda x: x[1], probs)), list(map(lambda x: x[0], probs))
-            probs = np.array(probs) / temperature
-            probs = probs - np.max(probs)
-            probs = np.exp(probs)
-            probs = probs / np.sum(probs)
-            next_token = np.random.choice(indices, p=probs)
-            input_data[index].append(0)
-            input_data[index][text_lens[index] + shift] = next_token
-    outputs = [bpe.decode(input_data[index][:text_lens[index] + length]) for index in range(batch_size)]
+    batch_size = len(texts) # if bsz==64
+    encodes = [bpe.encode(text) for text in texts] # len 64, different length of token lists form this list
+    text_lens = [len(encode) for encode in encodes] #[len1, len2, .. len54]
+    max_len = max(text_lens) # get maximum of all lens
+    input_data = [encode + [0] * (max_len - len(encode)) for encode in encodes] #padding to make align
+    for shift in range(length): # from 0 - 99th adding tokens (add one, shift one to deal with next position)
+        output_data = model.predict(np.array(input_data)) # predict a matrix with shape [64, 50257]
+        for index in range(batch_size): # every sample
+            probs = [(prob, i) for i, prob in enumerate(output_data[index, text_lens[index] + shift - 1])] # get the last token output with shape [1,50257]
+            probs.sort(reverse=True) # sort with largest first
+            probs = probs[:top_k] # get top k choices
+            indices, probs = list(map(lambda x: x[1], probs)), list(map(lambda x: x[0], probs)) #seperate indice and the corresponding prob into 2 diff lists
+            probs = np.array(probs) / temperature # deal 1 with new probs list, smoothing
+            probs = probs - np.max(probs) # deal 2 with new probs list, get rid of potential too-large exponential
+            probs = np.exp(probs) # deal 3 with new probs list
+            probs = probs / np.sum(probs) # deal 4 with new probs list， with deals above is doing softmax
+            next_token = np.random.choice(indices, p=probs) # base on probability distribution to choose the token
+            input_data[index].append(0) # allocate a space in python
+            input_data[index][text_lens[index] + shift] = next_token # set the new space with the chosen token
+    outputs = [bpe.decode(input_data[index][:text_lens[index] + length]) for index in range(batch_size)] # get all result in str
     return outputs
